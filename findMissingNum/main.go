df128c9bf38d6e9a318d03a01a89ce7a09e14aa0
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2022-12-30 09:32:38 +0530

    find Missing Number in the array

--- findMissingNum/main.go
+++ findMissingNum/main.go
@@ -2,45 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-// FindMissing function will find the missing number in an inpur array
-func FindMissing(input []int) int {
+func missingElementsFromArray(arr []int, N int) []int {
+	result := []int{}
+	var diff = arr[0] - 0
 
-	actualSum := 0
-
-	for _, inp := range input {
-		actualSum += inp
+	for i := 0; i < N; i++ {
+		if arr[i]-i != diff {
+			for diff < arr[i]-i {
+				result = append(result, i+diff)
+				diff++
+			}
+		}
 	}
 
-	n := len(input)
-	sumOfN := float64(n*(n+1)) / float64(2)
-
-	return int(sumOfN - float64(actualSum))
-}
-
-func ArrayToString(nums []int) string {
-	if len(nums) == 0 {
-		return "[]"
-	}
-	res := "["
-	for _, num := range nums {
-		res += strconv.Itoa(num) + ", "
-	}
-	res += "]"
-	return res[:len(res)-3] + "]"
+	return result
 }
 
 func main() {
-	array := [][]int{
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 17, 19},
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 17, 19},
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 17, 19},
-	}
-	for i, arr := range array {
-		fmt.Printf("%d.     Original: %s\n", i+1, ArrayToString(arr))
-		missingNumber := FindMissing(arr)
-		fmt.Printf("       The missing number is: %d\n", missingNumber)
-	}
+
+	arr := []int{1, 2, 6, 7, 10}
+	var N = len(arr)
+
+	res := missingElementsFromArray(arr, N)
+	fmt.Println("The missing numbers are:", res)
+
 }
