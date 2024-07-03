e940842d0316356496f98dde99fc88c4dd1d4c8e
nikhil upadhyay <80529715+nikhilupadhyay1709@users.noreply.github.com>
2024-07-03 17:22:47 +0530

    Update main.go

--- findPairsWithTheGivenSum/main.go
+++ findPairsWithTheGivenSum/main.go
@@ -1,22 +1,22 @@
 package main
 
-import "fmt"
-
-func findSumPair(arr []int, sum int) {
-	
-	m := make(map[int]int)
-
-	for i := 0; i < len(arr); i++ {
-		if m[sum-arr[i]] == 0 {
-			m[arr[i]] = i
-		} else {
-			fmt.Printf("Pair for given sum is (%d, %d).\n", arr[m[sum-arr[i]]], arr[i])
-			return
-		}
-	}
-}
+import (
+	"fmt"
+)
 
 func main() {
-	findSumPair([]int{4, 3, 6, 7, 8, 1, 9}, 15)
-	findSumPair([]int{4, 3, 6, 7, 8, 1, 9}, 100)
+	arr := []int{1, 4, 6, 7, 2, 8, 9, 9, 45, 67}
+	findPairSum(arr, 10)
+}
+
+func findPairSum(arr []int, sum int) {
+	m := make(map[int]bool)
+
+	for _, value := range arr {
+		complement := sum - value
+		if m[complement] {
+			fmt.Println("Given Pair:", complement, value)
+		}
+		m[value] = true
+	}
 }
