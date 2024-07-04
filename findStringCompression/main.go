fd9e217af54a382c0f897ca565b685a59f388669
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2024-07-04 22:54:01 +0530

    changes var name

--- findStringCompression/main.go
+++ findStringCompression/main.go
@@ -9,21 +9,21 @@ func main() {
 	s := "aabcccccaaa"
 	n := len(s)
 
-	var compressed string
+	var comp string
 	count := 1
 
 	for i := 1; i <= n; i++ {
 		if i < n && s[i] == s[i-1] {
 			count++
 		} else {
-			compressed += string(s[i-1]) + strconv.Itoa(count)
+			comp += string(s[i-1]) + strconv.Itoa(count)
 			count = 1
 		}
 	}
 
-	if len(compressed) >= n {
+	if len(comp) >= n {
 		fmt.Println(s)
 	} else {
-		fmt.Println(compressed)
+		fmt.Println(comp)
 	}
 }
