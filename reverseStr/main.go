cdef1cb999cebb7c3cd54513fc293afb88ee05f6
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2024-07-14 14:46:34 +0530

    Update logic for reverse string by words

--- reverseStr/main.go
+++ reverseStr/main.go
@@ -2,21 +2,13 @@ package main
 
 import "fmt"
 
-// function to reverse string
-func reverseString(str string) (res string) {
-	// iterate over str and prepend to result
+func main() {
+	str := "Hello, Go!"
+
+	var res string
 	for _, v := range str {
 		res = string(v) + res
 	}
 
-	return
-}
-
-func main() {
-	str := "Nikhil"
-
-	fmt.Println("Original String 🚀:", str)
-	// invoke reverseString
-	fmt.Println("Reverse String 🚀:", reverseString(str))
-
+	fmt.Println("Reverse String 🚀:", res)
 }
