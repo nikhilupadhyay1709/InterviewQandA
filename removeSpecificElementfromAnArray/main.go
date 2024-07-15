d967d107c02300c5a10e2a0e849b16db2171602c
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2024-07-15 15:49:33 +0530

    update the function

--- removeSpecificElementfromAnArray/main.go
+++ removeSpecificElementfromAnArray/main.go
@@ -2,21 +2,16 @@ package main
 
 import "fmt"
 
-func removeElement(arr []int, e int) []int {
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	e := 3
 
 	var res []int
-
 	for _, v := range arr {
 		if v != e {
 			res = append(res, v)
 		}
 	}
 
-	return res
-}
-
-func main() {
-
-	arr := []int{1, 2, 3, 4, 5}
-	fmt.Println("Array after removing :", removeElement(arr, 3))
+	fmt.Println("Array after removing:", res)
 }
