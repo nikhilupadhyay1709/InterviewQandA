cd595efc04f5d964f0ab486d22fadd026e017983
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2025-04-19 22:43:53 +0530

    Update: few more main.go

--- pool/main.go
+++ pool/main.go
@@ -6,34 +6,34 @@ import (
 )
 
 // Worker function processes tasks from the jobs channel.
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, jobs <-chan int, res chan<- int) {
 	for job := range jobs {
 		fmt.Printf("Worker %d is processing job %d\n", id, job)
 		time.Sleep(time.Second) // Simulate work
-		results <- job * 2      // Send result back
+		res <- job * 2          // Send result back
 	}
 }
 
 func main() {
-	numJobs := 5      // Number of tasks
-	numWorkers := 3   // Number of workers
+	njobs := 5      // Number of tasks
+	numWorkers := 3 // Number of workers
 
-	jobs := make(chan int, numJobs)    // Task queue
-	results := make(chan int, numJobs) // Results queue
+	jobs := make(chan int, njobs) // Task queue
+	res := make(chan int, njobs)  // res queue
 
 	// Start workers
 	for w := 1; w <= numWorkers; w++ {
-		go worker(w, jobs, results)
+		go worker(w, jobs, res)
 	}
 
 	// Send jobs to the jobs channel
-	for j := 1; j <= numJobs; j++ {
+	for j := 1; j <= njobs; j++ {
 		jobs <- j
 	}
 	close(jobs) // Close the jobs channel when done sending tasks
 
-	// Collect results
-	for r := 1; r <= numJobs; r++ {
-		fmt.Printf("Result: %d\n", <-results)
+	// Collect res
+	for r := 1; r <= njobs; r++ {
+		fmt.Printf("Result: %d\n", <-res)
 	}
 }
