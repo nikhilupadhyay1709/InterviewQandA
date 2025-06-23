9dd6f895c8bfab9a59c441771ecc5dae8e86b17a
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2025-06-23 20:40:54 +0530

    optim the code

--- findSecondMinNum/main.go
+++ findSecondMinNum/main.go
@@ -7,28 +7,16 @@ import (
 
 func main() {
 	arr := []int{1, 2, 3, 10}
-	secondMinNum := findSecondMinNum(arr)
-	if secondMinNum == math.MaxInt64 {
-		fmt.Println("No second minimum value found")
-	} else {
-		fmt.Println("secondMinNum:", secondMinNum)
-	}
-}
-
-func findSecondMinNum(arr []int) int {
-	if len(arr) < 2 {
-		return math.MaxInt64
-	}
 
 	min, secondMin := math.MaxInt64, math.MaxInt64
 
-	for _, num := range arr {
-		if num < min {
-			secondMin, min = min, num
-		} else if num < secondMin && num != min {
-			secondMin = num
+	for _, v := range arr {
+		if v < min {
+			secondMin, min = min, v
+		} else if v < secondMin && v != min {
+			secondMin = v
 		}
 	}
 
-	return secondMin
+	fmt.Printf("The second minimum number in the array is: %d\n", secondMin)
 }
