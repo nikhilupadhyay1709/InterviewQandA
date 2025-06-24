27ee8f92a352284f1d49bf7a94e1e8cfb62d7165
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2025-06-24 20:29:18 +0530

    updated var name

--- palidromeString/main.go
+++ palidromeString/main.go
@@ -5,17 +5,15 @@ import "fmt"
 func main() {
 	str := "NITIN"
 	str = "Nikhil"
-
 	fmt.Println("Is Palindrome 🚀:", IsPalindrome(str))
-
 }
 
 func IsPalindrome(str string) bool {
-	result := []byte{}
+	res := []byte{}
 
 	for i := len(str) - 1; i >= 0; i-- {
-		result = append(result, str[i])
+		res = append(res, str[i])
 	}
-	
-	return str == string(result)
+
+	return str == string(res)
 }
