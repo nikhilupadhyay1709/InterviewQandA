304730024a343c72a2f5873b87f4820f273d95bb
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2025-07-22 17:03:08 +0530

    added: new question and asnwer

--- stringContainsNum/main.go
+++ stringContainsNum/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 )
 
 func main() {
-	word := "test25"
-	numeric := regexp.MustCompile(`\d`).MatchString(word)
-	fmt.Println(numeric)
+	word := "this is 23 nikhil 17 updhaay89 here for interview314"
+	num := getNumsInString(word)
+	fmt.Println("array before the sum :", num)
+	res := sum(num)
+	fmt.Println("res after the sum", res)
 }
 
+func sum(arr []int) int {
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+
+	return sum
+}
+
+func getNumsInString(s string) []int {
+	var nums []int
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			nums = append(nums, int(c-'0'))
+		}
+	}
+	return nums
+}
