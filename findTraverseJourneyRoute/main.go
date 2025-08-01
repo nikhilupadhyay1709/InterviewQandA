35b658722cd927467ecbfcb7ef5625405c4127c5
nikhilupadhyay1709 <nikhilupadhyay1709@gmail.com>
2025-08-01 17:57:12 +0530

    traverse a journey route

---
+++ findTraverseJourneyRoute/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Position struct {
+	Start string `json:"start"`
+	End   string `json:"end"`
+}
+
+func main() {
+	str := []Position{
+		{Start: "Home", End: "Busstop"},
+		{Start: "Office", End: "Gym"},
+		{Start: "Gym", End: "Home"},
+		{Start: "Busstop", End: "Office"},
+	}
+
+	res := findStartToEndLocation(str, "Home")
+	fmt.Println(strings.Join(res, " -> "))
+}
+
+func findStartToEndLocation(routes []Position, start string) []string {
+	// Create a map from start to end
+	routeMap := make(map[string]string)
+	for _, pos := range routes {
+		routeMap[pos.Start] = pos.End
+	}
+
+	var path []string
+	current := start
+
+	for {
+		path = append(path, current)
+		next, exists := routeMap[current]
+		if !exists || next == start {
+			// Either route ends or comes back to start
+			if exists {
+				path = append(path, start)
+			}
+			break
+		}
+		current = next
+	}
+
+	return path
+}
